7b49c975c86a066d87ed9e5cd3390c06b40ec0fb
dishanth <shettysamarth738@gmail.com>
2023-11-09 22:20:28 +0530

    added few changes

--- index.js
+++
@@ -1,7 +0,0 @@
-
-// JavaScript for adding the product to the cart
-const addToCartButton = document.getElementById("add-to-cart");
-addToCartButton.addEventListener("click", function() {
-    alert("Product added to cart!");
-    // You can add code to update the shopping cart here.
-});
